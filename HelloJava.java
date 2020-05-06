54833c001b0a6582efb34bbb4081c774756ee877
Ubuntu <hpgpune@yahoo.co.in>
2020-05-06 12:49:07 +0000

    my files to github

---
+++ HelloJava.java
@@ -0,0 +1,4 @@
+class HelloJava{
+public static void main(String args[]){
+System.out.Println(" i am your java program");}
+}
---
+++ c_program.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+printf("hello ! I am a c-program" );
+return 0
+}
